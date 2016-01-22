3cc19a6645a54689e314293a80ebfd18b111772c
Charles Carter <charles.h.carter@gmail.com>
2016-01-22 14:27:37 -0800

    Initialized css, js, index. Began initial layout of form.

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+  $("form#vacationrec").submit(function(event) {
+    // capturing data of age, temp, and distance from form
+    var age = parseInt($("input#age").val());
+    var temperature = parseInt($("input#temperature").val());
+    var distance = parseInt($("input#distance").val());
+    // capturing food/transport data from radio and checkbox in form
+    var 
+
+    
+    // Algorithm that does all this... Attempt 1: Organize by distance
+    // if Distance is -infinity and +10 alert msg (You may try a staycation!)
+    // if 
+  }
+
+
+});
+
+// Algorithm that does all this...
