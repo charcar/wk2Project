bb1f5ac9c843129c0577dbece9eab4209e802525
Charles Carter <charles.h.carter@gmail.com>
2016-01-22 21:51:45 -0800

    troubleshot algorithm without much success, styled answers

--- js/scripts.js
+++ js/scripts.js
@@ -9,28 +9,33 @@ $(document).ready(function() {
     var bland = $("bland#bland").val();
     var vacuum = $("vacuum#vacuum").val();
 
-    var car = $("car#car").val();
-    var walk = $("walk#walk").val();
-    var hippie = $("hippie#hippie").val();
+    // These proved to be unnecessary. I still haven't figured out how to capture data from radio fields,
+    // and then how to incorporate that into the script. I know its a matter of syntax and data types,
+    // but I've had an unsuccessful time with it, and I've decided to move on.
+    // var car = $("car#car").val();
+    // var walk = $("walk#walk").val();
+    // var hippie = $("hippie#hippie").val();
 
     // var x = document.getElementById("myRadio").value;
 
     
-    // Attempt 1: Organize by distance
+    // Main logic: Organized by distance, from there destinations vary based on other factors entered
     
-    // if Distance is -infinity and +25 alert msg (You may try a staycation!)
     if (distance < 25) {
-      alert("Wny don't you consider just a 'staycation'?")
+      alert("Wny don't you consider just a 'staycation'?");
     }
       // if Distance is 26-500
       // check food, if mild -> John Day
       // if spicy -> Vancouver
       else if (distance > 26 && distance < 500) {
-        if ($("input:radio[name='spicy']").is(":checked")) {
         $("#Vancouver").show();
-    } 
-      else {
-        $("#JohnDay").show();
+        if ($("#spicy").attr("checked") === "checked") {
+        }
+        // if ($("input:radio[name='spicy']").is(":checked")) {
+        // $("#Vancouver").show();
+     
+        else if (age > 40) {
+          $("#JohnDay").show();
     }
     }
       // if distance 501-2600
@@ -56,7 +61,7 @@ $(document).ready(function() {
           $("#Iceland").show();
         }
         else {
-          alert("Wait, let me get this straight. You want to travel thousands of miles away from home, just to walk around freezing your buns off? You are one special flower.")
+          alert("Wait, let me get this straight. You want to travel thousands of miles away from home, just to walk around freezing your buns off? You are one special flower.");
         }
       }
       // last else addresses when distance >4000
