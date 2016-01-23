d829bdf588668e2e87fb93b463bb3ddc536e8286
Charles Carter <charles.h.carter@gmail.com>
2016-01-22 16:31:22 -0800

    first finished copy of recommendation algorithm

--- js/scripts.js
+++ js/scripts.js
@@ -19,27 +19,59 @@ $(document).ready(function() {
     // Attempt 1: Organize by distance
     
     // if Distance is -infinity and +25 alert msg (You may try a staycation!)
-
-    // if Distance is 26-500
+    if (distance < 25) {
+      alert("Wny don't you consider just a 'staycation'?")
+    }
+      // if Distance is 26-500
       // check food, if mild -> John Day
-        // if spicy -> Vancouver
-    // if distance 501-2600
+      // if spicy -> Vancouver
+      else if (distance > 26 && distance < 500) {
+        if ($("input:radio[name='spicy']").is(":checked")) {
+        $("#Vancouver").show();
+    } 
+      else {
+        $("#JohnDay").show();
+    }
+    }
+      // if distance 501-2600
       // check temp, if 0-55 -> Minnesota
-        // if 56-100 -> Nola
-    // if distance 2601-4000
+      // if 56-100 -> Nola 
+      else if (distance > 500 && distance < 2600 ) {
+        if (temperature > -41 && temperature < 56) {
+          $("#Minnesota").show();
+    } 
+        else {
+          $("#Nola").show();
+    }
+    }
+      // if distance 2601-4000
       // if temp 65-100 -> Puerto Rico
-        // if temp 0-64
-          // if car === true -> Iceland
-          // else (i.e. foot===true) -> alert("Wait let me get this straight, you want to travel thousands of miles to walk around in the cold?")
-      
+      // if temp 0-64
+      else if (distance > 2600 && distance < 4000) {
+        if (temperature > 60 && temperature < 100) {
+          $("#PuertoRico").show();
+        }
+      // if vehicle is checked show Iceland
+        else if ($("input:radio[name='car']").is(":checked")) {
+          $("#Iceland").show();
+        }
+        else {
+          alert("Wait, let me get this straight. You want to travel thousands of miles away from home, just to walk around freezing your buns off? You are one special flower.")
+        }
+      }
+      // last else addresses when distance >4000
+    else { 
+      alert("Go to New Zealand. Trust me. Just go.")
+    } 
+  
 
-    // these will go in the algorithm
-    $("#JohnDay").show();
-    $("#Vancouver").show();
-    $("#Nola").show();
-    $("#Minnesota").show();
-    $("#PuertoRico").show();
-    $("#Iceland").show();
+    // these will go in the algorithm, and only get shown when the proper conditions are fulfilled, commented out when inserted into code
+    // $("#JohnDay").show();
+    // $("#Vancouver").show();
+    // $("#Nola").show();
+    // $("#Minnesota").show();
+    // $("#PuertoRico").show();
+    // $("#Iceland").show();
 
     event.preventDefault();
 
